aa751e2ca7acc7ef5114b4aada1fcfda44b31dcd
Shandon Mith <Shandonmith@gmail.com>
2022-08-25 22:12:37 -0400

    setup server

---
+++ src/main/java/shando/com/tutorials/tutorials/model/Tutorial.java
@@ -0,0 +1,16 @@
+package shando.com.tutorials.tutorials.model;
+import javax.persistence.*;
+@Entity
+@Table(name = "tutorials")
+
+public class Tutorial {
+  @Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private long id;
+	@Column(name = "title")
+	private String title;
+	@Column(name = "description")
+	private String description;
+	@Column(name = "published")
+	private boolean published;
+}
---
+++ src/main/java/shando/com/tutorials/tutorials/repository/TutorialRepository.java
@@ -0,0 +1,12 @@
+package shando.com.tutorials.tutorials.repository;
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import shando.com.tutorials.tutorials.model.Tutorial;
+
+public interface TutorialRepository extends JpaRepository<Tutorial, Long> {
+  List<Tutorial> findByPublished(boolean published);
+  List<Tutorial> findByTitleContaining(String title);
+}
